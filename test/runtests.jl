dc35d8e2821e1fe7a813647e3302d631e188a462
Uwe Fechner <u.fechner@tudelft.nl>
2015-08-19 12:30:28 +0200

    Added the functions maximum, minimum, mean, var and std.

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,3 +8,8 @@ using Base.Test
 @test isnan(NaNMath.sum([NaN, NaN]))
 @test NaNMath.sum(Float64[]) == 0.0
 @test NaNMath.sum([1f0, 2f0, NaN32]) === 3.0f0
+@test NaNMath.maximum([1., 2., NaN]) == 2.0
+@test NaNMath.minimum([1., 2., NaN]) == 1.0
+@test NaNMath.mean([1., 2., NaN]) == 1.5
+@test NaNMath.var([1., 2., NaN]) == 0.5
+@test NaNMath.std([1., 2., NaN]) == 0.7071067811865476
